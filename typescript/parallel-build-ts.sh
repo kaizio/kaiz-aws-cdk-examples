63124606c7641774eb3a0972569a44e0a73922af
Michael Kaiser <mjkaiser@gmail.com>
2023-10-04 12:42:50 -0500

    Fix: Add script that I run

--- typescript/parallel-build-ts.sh
+++ typescript/parallel-build-ts.sh
@@ -1,3 +1,4 @@
+# find ./ -iname cdk.json | parallel -j 4 sh ./parallel-build-ts.sh {}
 cdk_json_path=$1
 
 cdk_app_path=$(dirname $cdk_json_path)
@@ -8,6 +9,6 @@ cd $cdk_app_path
 
 npm install > /dev/null
 
-npm run build > /dev/null
+#npm run build > /dev/null
 
 npx cdk synth > /dev/null
