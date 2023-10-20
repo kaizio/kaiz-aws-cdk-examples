f1f8a1cab254926a3fa8f3ec1ae0b03495642508
Michael Kaiser <mjkaiser@gmail.com>
2023-10-20 14:48:34 -0500

    Parse aws-cdk and constructs

--- scripts/update-cdk-ts-packages.sh
+++ scripts/update-cdk-ts-packages.sh
@@ -1,7 +1,7 @@
 # Download files from aws
 curl -sL -o other-packages.json https://raw.githubusercontent.com/aws/aws-cdk/main/packages/aws-cdk/lib/init-templates/app/typescript/package.json
-curl -sL -o cdk-version.json  https://raw.githubusercontent.com/aws/aws-cdk/main/version.v2.json
-curl -sL -o constructs.json https://raw.github.com/aws/aws-cdk/packages/aws-cdk/package.json
+curl -sL -o cdk-version.json https://raw.githubusercontent.com/aws/aws-cdk/main/version.v2.json
+curl -sL -o constructs.json https://raw.githubusercontent.com/aws/aws-cdk/main/packages/aws-cdk/package.json
 
 # Find all the package.json files in the typescript/ directory
 files=$(find ../typescript/ -iname package.json -not -path "*/node_modules/*")
@@ -31,11 +31,15 @@ typescript=$(cat other-packages.json | jq -r '.devDependencies.typescript')
 echo $files | xargs sed -i 's/\("typescript": "\)\(.*\)\(",\?\)/\1'"$typescript"'\3/g'
 
 # Get current sourcemap and update all package.json files
-sourcemap=$(cat other-packages.json | jq -r '.Dependencies."source-map-support"')
+sourcemap=$(cat other-packages.json | jq -r '.dependencies."source-map-support"')
 echo $files | xargs sed -i 's/\("source-map-support": "\)\(.*\)\(",\?\)/\1'"$sourcemap"'\3/g'
 
 # Update AWS CDK versions for aws-cdk and aws-cdk-lib
 
-echo $files | xargs sed -i 's/\("aws-cdk": "\)\(.*\)\(",\?\)/\12.99.1\3/g'
-echo $files | xargs sed -i 's/\("aws-cdk-lib": "\)\(.*\)\(",\?\)/\12.99.1\3/g'
-echo $files | xargs sed -i 's/\("constructs": "\)\(.*\)\(",\?\)/\1^10.0.0\3/g'
+cdkversion=$(cat cdk-version.json | jq -r '.version')
+echo $files | xargs sed -i 's/\("aws-cdk": "\)\(.*\)\(",\?\)/\1'"$cdkversion"'\3/g'
+echo $files | xargs sed -i 's/\("aws-cdk-lib": "\)\(.*\)\(",\?\)/\1'"$cdkversion"'\3/g'
+
+# Get current constructs version and update all package.json files
+constructs=$(cat constructs.json | jq -r '.devDependencies.constructs')
+echo $files | xargs sed -i 's/\("constructs": "\)\(.*\)\(",\?\)/\1'"$constructs"'\3/g'
