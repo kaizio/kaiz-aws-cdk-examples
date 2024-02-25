d5887d5569c16de721412bf6181f0a0e63dca61b
Michael Kaiser <mjkaiser@gmail.com>
2024-02-25 09:50:36 -0600

    Merge: Commit from master

---
+++ typescript/updatepackages.sh
@@ -0,0 +1,11 @@
+find ./ -iname package.json -not -path "*/node_modules/*"
+find ./ -iname package.json -not -path "*/node_modules/*" -exec sed -i 's/\("@types\/jest": "\)\(.*\)\(",\?\)/\1^29.5.5\3/g' {} \;
+find ./ -iname package.json -not -path "*/node_modules/*" -exec sed -i 's/\("@types\/node": "\)\(.*\)\(",\?\)/\120.6.3\3/g' {} \;
+find ./ -iname package.json -not -path "*/node_modules/*" -exec sed -i 's/\("jest": "\)\(.*\)\(",\?\)/\1^29.7.0\3/g' {} \;
+find ./ -iname package.json -not -path "*/node_modules/*" -exec sed -i 's/\("ts-jest": "\)\(.*\)\(",\?\)/\1^29.1.1\3/g' {} \;
+find ./ -iname package.json -not -path "*/node_modules/*" -exec sed -i 's/\("aws-cdk": "\)\(.*\)\(",\?\)/\12.99.1\3/g' {} \;
+find ./ -iname package.json -not -path "*/node_modules/*" -exec sed -i 's/\("ts-node": "\)\(.*\)\(",\?\)/\1^10.9.1\3/g' {} \;
+find ./ -iname package.json -not -path "*/node_modules/*" -exec sed -i 's/\("typescript": "\)\(.*\)\(",\?\)/\1~5.2.2\3/g' {} \;
+find ./ -iname package.json -not -path "*/node_modules/*" -exec sed -i 's/\("aws-cdk-lib": "\)\(.*\)\(",\?\)/\12.99.1\3/g' {} \;
+find ./ -iname package.json -not -path "*/node_modules/*" -exec sed -i 's/\("constructs": "\)\(.*\)\(",\?\)/\1^10.0.0\3/g' {} \;
+find ./ -iname package.json -not -path "*/node_modules/*" -exec sed -i 's/\("source-map-support": "\)\(.*\)\(",\?\)/\1^0.5.21\3/g' {} \;
