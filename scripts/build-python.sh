d38c288379ed4fdfefe04b772ffadc6ffbb0f83c
Michael Kaiser <kaiser@kaiz.io>
2024-05-13 00:00:09 -0500

    Fix: venv path

--- scripts/build-python.sh
+++ scripts/build-python.sh
@@ -9,16 +9,15 @@ echo "building project: $projFile"
 echo "=============================="
 
 cd $scriptdir/../$(dirname $projFile)
-echo "$(tput bold)Building project at $(dirname $projFile)$(tput sgr0)"
 [[ ! -f DO_NOT_AUTOTEST ]] || exit 0
 
-python3 -m venv /tmp/.venv
+python3 -m venv .venv
 
-source /tmp/.venv/bin/activate
+source .venv/bin/activate
 pip install -r requirements.txt
 
 $scriptdir/synth.sh
 # It is critical that we clean up the pip venv before we build the next python project
 # Otherwise, if anything gets pinned in a requirements.txt, you end up with a weird broken environment
-rm -rf /tmp/.venv
+rm -rf .venv
 echo "::endgroup::"
